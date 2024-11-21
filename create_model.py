db50db1af4ebb013917bcccaaf436842d70c0a8e
Sayantan Nath <164194164+sayantan2025@users.noreply.github.com>
2024-11-21 18:41:32 +0530

    Update create_model.py

--- create_model.py
+++ create_model.py
@@ -1,9 +1,10 @@
-import pickle
+
+
+from joblib import dump
+from sklearn.linear_model import LinearRegression  # Replace with your actual model
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-import numpy as np
 import pandas as pd
-
 
 data = pd.read_csv('Data.csv')
 
@@ -21,9 +22,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.35, random
 model = LinearRegression()
 model.fit(X_train, y_train)
 
-
-# Save the model to a file
-with open("model.pkl", "wb") as file:
-    pickle.dump(model, file)
-
-print("Model saved as model.pkl")
+# Save the model using joblib
+dump(model, 'model.joblib')  # Save to current directory
+print("Model saved successfully!")
